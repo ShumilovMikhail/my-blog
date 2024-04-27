280a65748d347890f2b4533fab518257a3da8eaa
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-04-27 18:51:21 +0300

    add contact page

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    ContactComponent
   ],
   imports: [
     CommonModule
-  ],
-  exports: [FooterComponent]
+  ]
 })
-export class SharedModule { }
+export class ContactModule { }
